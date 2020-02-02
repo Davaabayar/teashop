d299d282743b6c8cb07e785d87458880f1ee5602
Tuvshintur <tuvshinturbatzorig@gmail.com>
2020-02-01 20:48:03 -0600

    route blog

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ let db;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const blogRouter = require('./routes/blogRoute');
 
 const app = express();
 
@@ -17,22 +18,22 @@ const app = express();
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(async (req, res, next) => {
 	try {
 		if (!db) {
-			await MongoClient.connect(dburl, {promiseLibrary: Promise})
-			.catch(err => {
-				console.error(err.stack);
-				return next(err);
-			})
-			.then(client => {
-				console.log("Db connection successful");
-				db = client.db();
-			});
+			await MongoClient.connect(dburl, { promiseLibrary: Promise })
+				.catch(err => {
+					console.error(err.stack);
+					return next(err);
+				})
+				.then(client => {
+					console.log("Db connection successful");
+					db = client.db();
+				});
 		}
 		req.db = db;
 		next();
@@ -43,6 +44,7 @@ app.use(async (req, res, next) => {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/blogs', blogRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -54,7 +56,7 @@ app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
-	
+
 	// render the error page
 	res.status(err.status || 500);
 	console.error(err);
