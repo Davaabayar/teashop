913eba3b3498c2d8618f25006a8109a03f4b11b0
Tuvshintur <tuvshinturbatzorig@gmail.com>
2020-02-02 14:10:35 -0600

    removed blog service from backend and added get blog posts without pagination

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async function (req, res) {
+    let result = await req.db.collection('blog')
+        .find({ status: "published" }).sort({ date: -1 }).toArray();
+    res.send(result).status(200);
+});
 
 router.get('/:page', async function (req, res) {
     let resultPerPage = 10;
@@ -8,7 +13,7 @@ router.get('/:page', async function (req, res) {
 
     let result = await req.db.collection('blog')
         .find({ status: "published" }).sort({ date: -1 }).skip((resultPerPage * page) - resultPerPage).limit(resultPerPage).toArray();
-    res.send(result);
+    res.send(result).status(200);
 });
 
 router.post('/', async function (req, res, next) {
