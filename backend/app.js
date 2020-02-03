c21436b579edf6068d233680bac030586f7045d0
Tuvshintur <tuvshinturbatzorig@gmail.com>
2020-02-03 11:17:06 -0600

    removed mongoose

--- backend/app.js
+++ backend/app.js
@@ -26,61 +26,61 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.use(async (req, res, next) => {
-dotenv.config();
-const dburl = "mongodb+srv://" + process.env.dbname + ":" + process.env.dbpass + "@cluster0-fetd1.mongodb.net/tshot";
-let db;
-
 app.use(async (req, res, next) => {
-	try {
-		if (!db) {
-			await MongoClient.connect(dburl, { promiseLibrary: Promise })
-				.catch(err => {
-					console.error(err.stack);
-					return next(err);
-				})
-				.then(client => {
-					console.log("Db connection successful");
-					db = client.db();
-				});
-		}
-		req.db = db;
-		next();
-	} catch (e) {
-		next(e);
-	}
-});*/
+	dotenv.config();
+	const dburl = "mongodb+srv://" + process.env.dbname + ":" + process.env.dbpass + "@cluster0-fetd1.mongodb.net/tshot";
+	let db;
 
-mongoose.connect("mongodb+srv://user3:user3@cluster0-fetd1.mongodb.net/tshot")
-	.then(()=>{
-		console.log('Database successfully connected.');
-	})
-	.catch((err)=>{
-		console.log('Database connection failed.');
-		console.log(err);
+	app.use(async (req, res, next) => {
+		try {
+			if (!db) {
+				await MongoClient.connect(dburl, { promiseLibrary: Promise })
+					.catch(err => {
+						console.error(err.stack);
+						return next(err);
+					})
+					.then(client => {
+						console.log("Db connection successful");
+						db = client.db();
+					});
+			}
+			req.db = db;
+			next();
+		} catch (e) {
+			next(e);
+		}
 	});
 
-app.use('/', indexRouter);
-app.use('/api/teas',teasRouter);
-app.use('/users', usersRouter);
-app.use('/shop', shopRoute);
-app.use('/blog', blogRouter)
-app.use('/user', usersRouter)
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	next(createError(404));
-});
+	// mongoose.connect("mongodb+srv://user3:user3@cluster0-fetd1.mongodb.net/tshot")
+	// 	.then(()=>{
+	// 		console.log('Database successfully connected.');
+	// 	})
+	// 	.catch((err)=>{
+	// 		console.log('Database connection failed.');
+	// 		console.log(err);
+	// 	});
 
-// error handler
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	app.use('/', indexRouter);
+	app.use('/api/teas', teasRouter);
+	app.use('/users', usersRouter);
+	app.use('/shop', shopRoute);
+	app.use('/blog', blogRouter)
+	app.use('/user', usersRouter)
+	// catch 404 and forward to error handler
+	app.use(function (req, res, next) {
+		next(createError(404));
+	});
 
-	// render the error page
-	res.status(err.status || 500);
-	console.error(err);
-	res.send(err);
-});
+	// error handler
+	app.use(function (err, req, res, next) {
+		// set locals, only providing error in development
+		res.locals.message = err.message;
+		res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-module.exports = app;
+		// render the error page
+		res.status(err.status || 500);
+		console.error(err);
+		res.send(err);
+	});
+
+	module.exports = app;
