1bb42ff7af320710243b769ebb5ee90e82e09c3c
Tuvshintur <tuvshinturbatzorig@gmail.com>
2020-02-03 23:19:00 -0600

    post details done

--- frontend/teashop/src/app/blog/services/post.service.ts
+++ frontend/teashop/src/app/blog/services/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from '../models/post';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ import { environment } from '../../../environments/environment';
 export class PostService {
 
   constructor(public http: HttpClient) { }
+
+  getOnePost(id: string): Observable<Post> {
+    return this.http.get<Post>(`${environment.serverURL}/api/blog/posts/one?id=` + id);
+  }
 
   getPosts(page: number) {
     return this.http.get(`${environment.serverURL}/api/blog/posts/` + page);
